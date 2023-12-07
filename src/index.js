4fa094efe480b61533eb97e2aca22ea333f88bb5
Bradford <phillip.g.bradford@gmail.com>
2023-12-07 14:19:15 -0500

    extra

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import Score from "./pages/Score";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+import HeaderData from "./data/HeaderData";
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+function App() {
+
+  const header_data = new HeaderData();
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="Quiz" element={<Quiz q_prop={"Hello"} />} />
+          <Route path="Score" element={<Score s_prop={ header_data} />} />
+          <Route path="Contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+root.render(<App />);
