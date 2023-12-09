400866d6fb2b7ba0d3bc2a708d75255f9a6d44b2
elsaepiccccc <113567701+elsaepiccccc@users.noreply.github.com>
2023-12-08 23:21:42 -0500

    Update Score.js

--- src/pages/Score.js
+++ src/pages/Score.js
@@ -2,16 +2,20 @@ import React from 'react';
 import my_state from './my_state';
 
 class Score extends React.Component {
-  
+
     render() {
+        // Inline CSS for text shadow
+        const headerStyle = {
+            textShadow: '2px 2px 4px #000000'  // Example: black shadow with 2px horizontal and vertical offset, and 4px blur
+        };
+
         return(
             <>
-                <h1>This is the Score page</h1>
-                <h2>{this.props.s_prop}</h2>
-                <h2> Your score so far is {my_state.my_score} / {my_state.my_count} </h2>
+                <h1 style={headerStyle}>Score Page</h1>
+
+                <h2> Your score is: {my_state.my_score} / {my_state.my_count} </h2>
                 <br/>
                 <br/>
-               
             </>
         );
     }
